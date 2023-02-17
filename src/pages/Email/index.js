34e9e8340890304dbb9bbeeaf06d27f98205e815
Vinicios-Pires <contato.viniciospires@gmail.com>
2023-02-17 01:41:58 -0300

    feat: preparations inbox

--- src/pages/Email/index.js
+++ src/pages/Email/index.js
@@ -3,47 +3,58 @@ import { TextField } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { GENERATE_EMAIL } from "../../utils/Client_apollo";
 
-export default function Email(){
-  const [emailText, setEmailText] = useState('');
+export default function Email() {
+	const [emailText, setEmailText] = useState("");
 
-  const [handleGenerateEmail] = useMutation(GENERATE_EMAIL, {
-    onCompleted: (el) => {
-      sessionStorage.setItem('@SESSION_ID', el.introduceSession.id)
-      sessionStorage.setItem('@TEMP_EMAIL', el.introduceSession.addresses[0].address)
-      setEmailText(sessionStorage.getItem('@TEMP_EMAIL'))
+	const [handleGenerateEmail] = useMutation(GENERATE_EMAIL, {
+		onCompleted: (el) => {
+			sessionStorage.setItem("@SESSION_ID", el.introduceSession.id);
+			sessionStorage.setItem(
+				"@TEMP_EMAIL",
+				el.introduceSession.addresses[0].address
+			);
+			setEmailText(sessionStorage.getItem("@TEMP_EMAIL"));
+		},
+		onError: (err) => {
+			console.log(err);
+		},
+	});
 
-      console.log(emailText)
-    },
-    onError: (err) => {
-      console.log(err)
-    }
-  })
+	useEffect(() => {
+		if (sessionStorage.getItem("@TEMP_EMAIL")) {
+			setEmailText(sessionStorage.getItem("@TEMP_EMAIL"));
+		}
+	}, []);
 
-  useEffect(() => {
-    if (sessionStorage.getItem('@TEMP_EMAIL')){
-      setEmailText(sessionStorage.getItem('@TEMP_EMAIL'))
-    }
-  }, [ ])
+	// function handleCopyEmail() {
+	// 	if (emailText.includes("@")) {
+	// 		navigator.clipboard.writeText(emailText);
+	// 		alert("Texto copiado!");
+	// 	} else {
+	// 		alert("Texto não copiado");
+	// 	}
+	// }
 
-  return (
-    <>
-      <h1>Pagina do Email</h1>
-      <form>
-        <TextField
-		    	id="email"
-		    	placeholder="Email"
-          value={emailText}
-		    	variant="outlined"
-          size="small"
-		    	inputProps={{
-		    		readOnly: true,
-		    	}}
-          onClick={handleGenerateEmail}
-		    >
-          {emailText}
-        </TextField>
-        {/* <button>Geral Email</button> */}
-      </form>
-    </>
-  )
+	return (
+		<>
+			<h1>Pagina do Email</h1>
+			<form>
+				<TextField
+					id="email"
+					placeholder="Email"
+					value={emailText}
+					variant="outlined"
+					size="small"
+					inputProps={{
+						readOnly: true,
+					}}
+					onClick={handleGenerateEmail}
+				>
+					{emailText}
+				</TextField>
+				{/* <button onClick={handleCopyEmail}>Copiar Email</button> */}
+			</form>
+			<div></div>
+		</>
+	);
 }
--- src/utils/Client_apollo.js
+++ src/utils/Client_apollo.js
@@ -1,29 +1,65 @@
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 
-const AUTH_TOKEN = "tokensuperseguroRS"
-const CORS = 'https://cors-anywhere.herokuapp.com/'
+const AUTH_TOKEN = "tokensuperseguroRS";
+const CORS = "https://cors-anywhere.herokuapp.com/";
 
 const client = new ApolloClient({
-  uri: `${CORS}https://dropmail.me/api/graphql/${AUTH_TOKEN}`,
-  fetchOptions: {
-    mode: 'cors',
-  },
-  headers: {
-    ContentType: 'text/json'
-  },
-  cache: new InMemoryCache(),
+	uri: `${CORS}https://dropmail.me/api/graphql/${AUTH_TOKEN}`,
+	fetchOptions: {
+		mode: "cors",
+	},
+	headers: {
+		ContentType: "text/json",
+	},
+	cache: new InMemoryCache(),
 });
 
 export const GENERATE_EMAIL = gql`
-  mutation {
-    introduceSession {
-      id,
-      expiresAt,
-      addresses {
-        address
+	mutation {
+		introduceSession {
+			id
+			expiresAt
+			addresses {
+				address
+			}
+		}
+	}
+`;
+
+export const CHECK_EMAIL = gql`
+  query {
+    session(id: "${sessionStorage.getItem("@SESSION_ID")}") {
+      mails {
+        rawSize,
+        fromAddr,
+        toAddr,
+        downloadUrl,
+        text,
+        headerSubject
       }
     }
   }
-`
+`;
+
+export function useQuery() {
+	const CHECK_EMAIL = gql`
+    query {
+      session(id: "${sessionStorage.getItem("@SESSION_ID")}") {
+        mails {
+          rawSize,
+          fromAddr,
+          toAddr,
+          downloadUrl,
+          text,
+          headerSubject
+        }
+      }
+    }
+  `;
+
+	const { data, errors, loading } = useQuery(CHECK_EMAIL);
+
+	return { data, errors, loading };
+}
 
 export default client;
