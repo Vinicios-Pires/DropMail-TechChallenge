f2eb9456952fde0cb307269669a2e7d48f18915a
Vinicios-Pires <contato.viniciospires@gmail.com>
2023-02-20 18:20:57 -0300

    cleaning the code

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -27,26 +27,6 @@ export default function Main() {
 	const [welcomeText, setWelcomeText] = useState("");
 	const [count, setCount] = useState(15);
 
-	(function () {
-		var cors_api_host = "cors-anywhere.herokuapp.com";
-		var cors_api_url = "https://" + cors_api_host + "/";
-		var slice = [].slice;
-		var origin = window.location.protocol + "//" + window.location.host;
-		var open = XMLHttpRequest.prototype.open;
-		XMLHttpRequest.prototype.open = function () {
-			var args = slice.call(arguments);
-			var targetOrigin = /^https?:\/\/([^/]+)/i.exec(args[1]);
-			if (
-				targetOrigin &&
-				targetOrigin[0].toLowerCase() !== origin &&
-				targetOrigin[1] !== cors_api_host
-			) {
-				args[1] = cors_api_url + args[1];
-			}
-			return open.apply(this, args);
-		};
-	})();
-
 	function permission() {
 		if (window.Notification.permission !== "granted") {
 			window.Notification.requestPermission((permission) => {
